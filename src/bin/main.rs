bd9df02ca706de2f87899138278a5540ff6e656e
Sylvan <sylvan@hey.com>
2021-07-11 14:55:20 +0200

    starting code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,284 +1,140 @@
-#![no_main]
 #![no_std]
+#![no_main]
 
-// set the panic handler
-use panic_halt as _;
-
-use core::convert::Infallible;
-use embedded_hal::digital::v2::{InputPin, OutputPin};
-use generic_array::typenum::{U13, U5};
-use keyberon::action::{k, l, m, Action, Action::*, HoldTapConfig};
-use keyberon::debounce::Debouncer;
-use keyberon::impl_heterogenous_array;
-use keyberon::key_code::KbHidReport;
-use keyberon::key_code::KeyCode::{self, *};
-use keyberon::layout::Layout;
-use keyberon::matrix::{Matrix, PressedKeys};
-use rtic::app;
-use stm32f4xx_hal::gpio::{self, gpioa, gpiob, Input, Output, PullUp, PushPull};
-use stm32f4xx_hal::otg_fs::{UsbBusType, USB};
-use stm32f4xx_hal::prelude::*;
-use stm32f4xx_hal::{stm32, timer};
-use usb_device::bus::UsbBusAllocator;
-use usb_device::class::UsbClass as _;
-
-type UsbClass = keyberon::Class<'static, UsbBusType, Leds>;
-type UsbDevice = usb_device::device::UsbDevice<'static, UsbBusType>;
-
-pub struct Cols(
-    gpiob::PB14<Input<PullUp>>,
-    gpiob::PB15<Input<PullUp>>,
-    gpiob::PB5<Input<PullUp>>,
-    gpiob::PB6<Input<PullUp>>,
-    gpiob::PB7<Input<PullUp>>,
-    gpiob::PB8<Input<PullUp>>,
-    gpioa::PA5<Input<PullUp>>,
-    gpioa::PA6<Input<PullUp>>,
-    gpioa::PA7<Input<PullUp>>,
-    gpiob::PB0<Input<PullUp>>,
-    gpiob::PB1<Input<PullUp>>,
-    gpiob::PB10<Input<PullUp>>,
-    gpioa::PA0<Input<PullUp>>,
-);
-impl_heterogenous_array! {
-    Cols,
-    dyn InputPin<Error = Infallible>,
-    U13,
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-}
-
-pub struct Rows(
-    gpioa::PA4<Output<PushPull>>,
-    gpioa::PA3<Output<PushPull>>,
-    gpioa::PA2<Output<PushPull>>,
-    gpioa::PA1<Output<PushPull>>,
-    gpiob::PB9<Output<PushPull>>,
-);
-impl_heterogenous_array! {
-    Rows,
-    dyn OutputPin<Error = Infallible>,
-    U5,
-    [0, 1, 2, 3, 4]
-}
-
-const CUT: Action<()> = m(&[LShift, Delete]);
-const COPY: Action<()> = m(&[LCtrl, Insert]);
-const PASTE: Action<()> = m(&[LShift, Insert]);
-const L2_ENTER: Action<()> = HoldTap {
-    timeout: 200,
-    config: HoldTapConfig::HoldOnOtherKeyPress,
-    tap_hold_interval: 0,
-    hold: &l(2),
-    tap: &k(Enter),
-};
-const L1_SP: Action<()> = HoldTap {
-    timeout: 200,
-    config: HoldTapConfig::Default,
-    tap_hold_interval: 0,
-    hold: &l(1),
-    tap: &k(Space),
-};
-const CSPACE: Action<()> = m(&[LCtrl, Space]);
-
-const SHIFT_ESC: Action<()> = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::Default,
-    hold: &k(LShift),
-    tap: &k(Escape),
-};
-const CTRL_INS: Action<()> = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::Default,
-    hold: &k(LCtrl),
-    tap: &k(Insert),
-};
-const ALT_NL: Action<()> = HoldTap {
-    timeout: 200,
-    tap_hold_interval: 0,
-    config: HoldTapConfig::Default,
-    hold: &k(LAlt),
-    tap: &k(NumLock),
+use embedded_hal::digital::v2::Input;
+use {
+    core::{
+        panic::PanicInfo,
+        sync::atomic::{compiler_fence, Ordering},
+    },
+    hal::{
+        gpio::{Input, Level, Pin, PullUp},
+        gpiote::*,
+        ppi::{self, ConfigurablePpi, Ppi},
+    },
+    nrf52840_hal as hal,
+    rtic::cyccnt::U32Ext,
+    rtt_target::{rprintln, rtt_init_print},
 };
 
-macro_rules! s {
-    ($k:ident) => {
-        m(&[LShift, $k])
-    };
-}
-macro_rules! a {
-    ($k:ident) => {
-        m(&[RAlt, $k])
-    };
-}
-
-// The 13th column is the hardware button of the development board,
-// thus all the column is activated when the button is pushed. Because
-// of that, only one action is defined in the 13th column.
-#[rustfmt::skip]
-pub static LAYERS: keyberon::layout::Layers<()> = &[
-    &[
-        &[k(Grave),  k(Kb1),k(Kb2),k(Kb3),  k(Kb4),k(Kb5), k(Kb6),   k(Kb7),  k(Kb8), k(Kb9),  k(Kb0),   k(Minus),  k(Space)],
-        &[k(Tab),     k(Q), k(W),  k(E),    k(R), k(T),    k(Y),     k(U),    k(I),   k(O),    k(P),     k(LBracket)],
-        &[k(RBracket),k(A), k(S),  k(D),    k(F), k(G),    k(H),     k(J),    k(K),   k(L),    k(SColon),k(Quote)   ],
-        &[k(Equal),   k(Z), k(X),  k(C),    k(V), k(B),    k(N),     k(M),    k(Comma),k(Dot), k(Slash), k(Bslash)  ],
-        &[Trans,      Trans,k(LGui),k(LAlt),L1_SP,k(LCtrl),k(RShift),L2_ENTER,k(RAlt),k(BSpace),Trans,   Trans      ],
-    ], &[
-        &[k(F1),         k(F2),   k(F3), k(F4),     k(F5),    k(F6),k(F7),      k(F8),    k(F9),    k(F10), k(F11),  k(F12)],
-        &[Trans,         k(Pause),Trans, k(PScreen),Trans,    Trans,Trans,      k(BSpace),k(Delete),Trans,  Trans,   Trans ],
-        &[Trans,         Trans,   ALT_NL,CTRL_INS,  SHIFT_ESC,Trans,k(CapsLock),k(Left),  k(Down),  k(Up),  k(Right),Trans ],
-        &[k(NonUsBslash),k(Undo), CUT,   COPY,      PASTE,    Trans,Trans,      k(Home),  k(PgDown),k(PgUp),k(End),  Trans ],
-        &[Trans,         Trans,   Trans, Trans,     Trans,    Trans,Trans,      Trans,    Trans,    Trans,  Trans,   Trans ],
-    ], &[
-        &[Trans,    Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans    ],
-        &[s!(Grave),s!(Kb1),s!(Kb2),s!(Kb3),s!(Kb4),s!(Kb5),s!(Kb6),s!(Kb7),s!(Kb8),s!(Kb9),s!(Kb0),s!(Minus)],
-        &[ k(Grave), k(Kb1), k(Kb2), k(Kb3), k(Kb4), k(Kb5), k(Kb6), k(Kb7), k(Kb8), k(Kb9), k(Kb0), k(Minus)],
-        &[a!(Grave),a!(Kb1),a!(Kb2),a!(Kb3),a!(Kb4),a!(Kb5),a!(Kb6),a!(Kb7),a!(Kb8),a!(Kb9),a!(Kb0),a!(Minus)],
-        &[Trans,    Trans,  Trans,  Trans,  CSPACE, Trans,  Trans,  Trans,  Trans,  Trans,  Trans,  Trans    ],
-    ], &[
-        &[Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans, Trans, Trans ],
-        &[k(F1),k(F2),k(F3),k(F4),k(F5),k(F6),k(F7),k(F8),k(F9),k(F10),k(F11),k(F12)],
-        &[Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans, Trans, Trans ],
-        &[Action::Custom(()),Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans, Trans, Trans ],
-        &[Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans,Trans, Trans, Trans ],
-    ],
-];
-
-pub struct Leds {
-    caps_lock: gpio::gpioc::PC13<gpio::Output<gpio::PushPull>>,
-}
-impl keyberon::keyboard::Leds for Leds {
-    fn caps_lock(&mut self, status: bool) {
-        if status {
-            self.caps_lock.set_low().unwrap()
-        } else {
-            self.caps_lock.set_high().unwrap()
-        }
-    }
-}
-
-#[app(device = stm32f4xx_hal::stm32, peripherals = true)]
+#[rtic::app(device = crate::hal::pac, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
 const APP: () = {
     struct Resources {
-        usb_dev: UsbDevice,
-        usb_class: UsbClass,
-        matrix: Matrix<Cols, Rows>,
-        debouncer: Debouncer<PressedKeys<U5, U13>>,
-        layout: Layout<()>,
-        timer: timer::Timer<stm32::TIM3>,
+        gpiote: Gpiote,
+        btn1: Pin<Input<PullUp>>,
+        btn3: Pin<Input<PullUp>>,
+        btn4: Pin<Input<PullUp>>,
     }
 
     #[init]
-    fn init(c: init::Context) -> init::LateResources {
-        static mut EP_MEMORY: [u32; 1024] = [0; 1024];
-        static mut USB_BUS: Option<UsbBusAllocator<UsbBusType>> = None;
+    fn init(mut ctx: init::Context) -> init::LateResources {
+        let _clocks = hal::clocks::Clocks::new(ctx.device.CLOCK).enable_ext_hfosc();
+        rtt_init_print!();
+        let p0 = hal::gpio::p0::Parts::new(ctx.device.P0);
+        let btn1 = p0.p0_11.into_pullup_input().degrade();
+        let btn2 = p0.p0_12.into_pullup_input().degrade();
+        let btn3 = p0.p0_24.into_pullup_input().degrade();
+        let btn4 = p0.p0_25.into_pullup_input().degrade();
+        let led1 = p0.p0_13.into_push_pull_output(Level::High).degrade();
 
-        let rcc = c.device.RCC.constrain();
-        let clocks = rcc
-            .cfgr
-            .use_hse(25.mhz())
-            .sysclk(84.mhz())
-            .require_pll48clk()
-            .freeze();
-        let gpioa = c.device.GPIOA.split();
-        let gpiob = c.device.GPIOB.split();
-        let gpioc = c.device.GPIOC.split();
+        let gpiote = Gpiote::new(ctx.device.GPIOTE);
 
-        let mut led = gpioc.pc13.into_push_pull_output();
-        led.set_low().unwrap();
-        let leds = Leds { caps_lock: led };
+        // Set btn1 to generate event on channel 0 and enable interrupt
+        gpiote
+            .channel0()
+            .input_pin(&btn1)
+            .hi_to_lo()
+            .enable_interrupt();
 
-        let usb = USB {
-            usb_global: c.device.OTG_FS_GLOBAL,
-            usb_device: c.device.OTG_FS_DEVICE,
-            usb_pwrclk: c.device.OTG_FS_PWRCLK,
-            pin_dm: gpioa.pa11.into_alternate_af10(),
-            pin_dp: gpioa.pa12.into_alternate_af10(),
-        };
-        *USB_BUS = Some(UsbBusType::new(usb, EP_MEMORY));
-        let usb_bus = USB_BUS.as_ref().unwrap();
+        // Set both btn3 & btn4 to generate port event
+        gpiote.port().input_pin(&btn3).low();
+        gpiote.port().input_pin(&btn4).low();
+        // Enable interrupt for port event
+        gpiote.port().enable_interrupt();
 
-        let usb_class = keyberon::new_class(usb_bus, leds);
-        let usb_dev = keyberon::new_device(usb_bus);
+        // PPI usage, channel 2 event triggers "task out" operation (toggle) on channel 1 (toggles led1)
+        gpiote
+            .channel1()
+            .output_pin(led1)
+            .task_out_polarity(TaskOutPolarity::Toggle)
+            .init_high();
+        gpiote.channel2().input_pin(&btn2).hi_to_lo();
+        let ppi_channels = ppi::Parts::new(ctx.device.PPI);
+        let mut ppi0 = ppi_channels.ppi0;
+        ppi0.set_task_endpoint(gpiote.channel1().task_out());
+        ppi0.set_event_endpoint(gpiote.channel2().event());
+        ppi0.enable();
 
-        let mut timer = timer::Timer::tim3(c.device.TIM3, 1.khz(), clocks);
-        timer.listen(timer::Event::TimeOut);
+        // Enable the monotonic timer (CYCCNT)
+        ctx.core.DCB.enable_trace();
+        ctx.core.DWT.enable_cycle_counter();
 
-        let matrix = Matrix::new(
-            Cols(
-                gpiob.pb14.into_pull_up_input(),
-                gpiob.pb15.into_pull_up_input(),
-                gpiob.pb5.into_pull_up_input(),
-                gpiob.pb6.into_pull_up_input(),
-                gpiob.pb7.into_pull_up_input(),
-                gpiob.pb8.into_pull_up_input(),
-                gpioa.pa5.into_pull_up_input(),
-                gpioa.pa6.into_pull_up_input(),
-                gpioa.pa7.into_pull_up_input(),
-                gpiob.pb0.into_pull_up_input(),
-                gpiob.pb1.into_pull_up_input(),
-                gpiob.pb10.into_pull_up_input(),
-                gpioa.pa0.into_pull_up_input(),
-            ),
-            Rows(
-                gpioa.pa4.into_push_pull_output(),
-                gpioa.pa3.into_push_pull_output(),
-                gpioa.pa2.into_push_pull_output(),
-                gpioa.pa1.into_push_pull_output(),
-                gpiob.pb9.into_push_pull_output(),
-            ),
-        );
+        rprintln!("Press a button");
 
         init::LateResources {
-            usb_dev,
-            usb_class,
-            timer,
-            debouncer: Debouncer::new(PressedKeys::default(), PressedKeys::default(), 5),
-            matrix: matrix.unwrap(),
-            layout: Layout::new(LAYERS),
+            gpiote,
+            btn1,
+            btn3,
+            btn4,
         }
     }
 
-    #[task(binds = OTG_FS, priority = 2, resources = [usb_dev, usb_class])]
-    fn usb_tx(mut c: usb_tx::Context) {
-        usb_poll(&mut c.resources.usb_dev, &mut c.resources.usb_class);
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        loop {
+            cortex_m::asm::wfi();
+        }
     }
 
-    #[task(binds = OTG_FS_WKUP, priority = 2, resources = [usb_dev, usb_class])]
-    fn usb_rx(mut c: usb_rx::Context) {
-        usb_poll(&mut c.resources.usb_dev, &mut c.resources.usb_class);
+    #[task(binds = GPIOTE, resources = [gpiote], schedule = [debounce])]
+    fn on_gpiote(ctx: on_gpiote::Context) {
+        if ctx.resources.gpiote.channel0().is_event_triggered() {
+            rprintln!("Interrupt from channel 0 event");
+        }
+        if ctx.resources.gpiote.port().is_event_triggered() {
+            rprintln!("Interrupt from port event");
+        }
+        // Reset all events
+        ctx.resources.gpiote.reset_events();
+        // Debounce
+        ctx.schedule.debounce(ctx.start + 3_000_000.cycles()).ok();
     }
 
-    #[task(binds = TIM3, priority = 1, resources = [usb_class, matrix, debouncer, layout, timer])]
-    fn tick(mut c: tick::Context) {
-        c.resources.timer.clear_interrupt(timer::Event::TimeOut);
+    #[task(resources = [gpiote, btn1, btn3, btn4])]
+    fn debounce(ctx: debounce::Context) {
+        let btn1_pressed = ctx.resources.btn1.is_low().unwrap();
+        let btn3_pressed = ctx.resources.btn3.is_low().unwrap();
+        let btn4_pressed = ctx.resources.btn4.is_low().unwrap();
 
-        for event in c
-            .resources
-            .debouncer
-            .events(c.resources.matrix.get().unwrap())
-        {
-            c.resources.layout.event(event);
+        if btn1_pressed {
+            rprintln!("Button 1 was pressed!");
+            // Manually run "task out" operation (toggle) on channel 1 (toggles led1)
+            ctx.resources.gpiote.channel1().out();
         }
-        match c.resources.layout.tick() {
-            keyberon::layout::CustomEvent::Release(()) => cortex_m::peripheral::SCB::sys_reset(),
-            _ => (),
+        if btn3_pressed {
+            rprintln!("Button 3 was pressed!");
+            // Manually run "task clear" on channel 1 (led1 on)
+            ctx.resources.gpiote.channel1().clear();
         }
-        send_report(c.resources.layout.keycodes(), &mut c.resources.usb_class);
+        if btn4_pressed {
+            rprintln!("Button 4 was pressed!");
+            // Manually run "task set" on channel 1 (led1 off)
+            ctx.resources.gpiote.channel1().set();
+        }
+    }
+
+    extern "C" {
+        fn SWI0_EGU0();
     }
 };
 
-fn send_report(iter: impl Iterator<Item = KeyCode>, usb_class: &mut resources::usb_class<'_>) {
-    use rtic::Mutex;
-    let report: KbHidReport = iter.collect();
-    if usb_class.lock(|k| k.device_mut().set_keyboard_report(report.clone())) {
-        while let Ok(0) = usb_class.lock(|k| k.write(report.as_bytes())) {}
+#[inline(never)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    cortex_m::interrupt::disable();
+    rprintln!("{}", info);
+    loop {
+        compiler_fence(Ordering::SeqCst);
     }
 }
 
-fn usb_poll(usb_dev: &mut UsbDevice, keyboard: &mut UsbClass) {
-    if usb_dev.poll(&mut [keyboard]) {
-        keyboard.poll();
-    }
-}
